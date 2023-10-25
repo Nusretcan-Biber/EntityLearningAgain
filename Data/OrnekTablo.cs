5766d42e7bc37c6551f31019e54444bcf286cdc5
Nusretcan-Biber <nusretcanbiber1@gmail.com>
2023-10-25 16:32:37 +0300

    Proje dosyası ekle.

---
+++ Data/OrnekTablo.cs
@@ -0,0 +1,10 @@
+﻿namespace Data
+{
+    public class OrnekTablo
+    {
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
---
+++ DataAccess/OrnekContext.cs
@@ -0,0 +1,23 @@
+﻿using Data;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace DataAccess
+{
+    public class OrnekContext :DbContext
+    {
+        public DbSet<OrnekTablo> OrnekTablos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrnekTablo>().ToTable(nameof(OrnekTablo));
+            modelBuilder.Entity<OrnekTablo>().HasKey(x => x.PersonId);
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Environment.GetEnvironmentVariable("ConnectionStrings"));
+        }
+
+    }
+}
