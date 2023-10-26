779857f0117540e71716f12159329b9d0f0d57db
Nusretcan-Biber <nusretcanbiber1@gmail.com>
2023-10-26 09:48:58 +0300

    GetEnviroment is not working YET

---
+++ DataAccess/Migrations/20231026063253_init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrnekTablos",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Surname = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrnekTablos", x => x.PersonId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrnekTablos");
+        }
+    }
+}
--- DataAccess/OrnekContext.cs
+++ DataAccess/OrnekContext.cs
@@ -9,14 +9,15 @@ namespace DataAccess
         public DbSet<OrnekTablo> OrnekTablos { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<OrnekTablo>().ToTable(nameof(OrnekTablo));
+         //   modelBuilder.Entity<OrnekTablo>().ToTable(nameof(OrnekTablo));
             modelBuilder.Entity<OrnekTablo>().HasKey(x => x.PersonId);
         }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite(Environment.GetEnvironmentVariable("ConnectionStrings"));
+            optionsBuilder.UseSqlite("Data Source=C:\\Program Files\\DB Browser for SQLite\\EntityLearning.db;");
+            //Environment.GetEnvironmentVariable("SQLITE_URI")
         }
 
     }
